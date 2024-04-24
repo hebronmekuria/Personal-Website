1d79e953e85d1bd14fa37f76b01bf764422494cf
hebronmekuria <hebron.mekuria@emu.edu>
2024-04-23 23:33:13 -0400

    Major updates in the main page file

--- hebron-mekuria/src/app/page.tsx
+++ hebron-mekuria/src/app/page.tsx
@@ -1,13 +1,95 @@
-import { Box, ChakraProvider, Heading, Text, VStack, HStack } from "@chakra-ui/react";
-import { InfiniteMovingCardsDemo } from "./components/MovingCardsDemo";
+import {
+  Box,
+  ChakraProvider,
+  Heading,
+  Image,
+  Img,
+  Text,
+  VStack,
+  HStack,
+} from "@chakra-ui/react";
 import { BasicCard } from "./components/BasicCard";
+import { TextCard } from "./components/TextCard";
+import { TracingBeam } from "./components/ui/tracing-beam";
+import { ProjectCard } from "./components/ProjectCard";
+import profile from "./static/profile.png";
+import { Vortex } from "./components/ui/vortex";
+import { VortexDemo } from "./components/VortexDemo";
 
 export default function Home() {
   return (
     <ChakraProvider>
-      <Box>
-        <BasicCard/>
+      
+      <Box alignContent='center'>
+        <TracingBeam className="px-6">
+          <Box w="full" minH="100vh">
+            <HStack spacing="24px">
+              <VStack spacing="5" minW="350px" me='100px'>
+                <Heading
+                  mt="30px"
+                  as="h1"
+                  color="#9A5CCB"
+                  fontWeight="semibold"
+                >
+                  Hebron Mekuria
+                </Heading>
+                <Text fontSize="20px" color="#000000" opacity="0.44">
+                  {" "}
+                  Back-End and Full-Stack Developer
+                </Text>
+                <Img
+                  borderRadius="full"
+                  mt="20px"
+                  boxSize="300px"
+                  src='https://bit.ly/dan-abramov'
+                  alt="Hebron Mekuria"
+                />
+              </VStack>
+              <TextCard/>
+            </HStack>
+
+            <HStack className="basic-card-bobbing" w="500px">
+              <BasicCard
+                title="Professional Experience"
+                subtitle="Past internships and jobs."
+                buttonUrl="https://example.com"
+              />
+              <BasicCard
+                title="Projects"
+                subtitle="Compilation of my coding projects."
+                buttonUrl="https://example.com"
+              />
+              <BasicCard
+                title="Skills"
+                subtitle="List of my technical and non-technical skills."
+                buttonUrl="https://example.com"
+              />
+              <BasicCard
+                title="Education"
+                subtitle="My undergraduate degree, activities and courses."
+                buttonUrl="https://example.com"
+              />
+            </HStack>
+          </Box>
+          <Box w="full" minH="100vh">
+            <Heading mt="30px" as="h1" color="#9A5CCB" fontWeight="semibold">
+              Projects
+            </Heading>
+          </Box>
+          <Box w="full" minH="100vh">
+            <Heading mt="30px" as="h1" color="#9A5CCB" fontWeight="semibold">
+              Professional Experiences
+            </Heading>
+          </Box>
+          <Box w="full" minH="100vh">
+            <Heading mt="30px" as="h1" color="#9A5CCB" fontWeight="semibold">
+              Skills
+              
+            </Heading>
+          </Box>
+        </TracingBeam>
       </Box>
+     
     </ChakraProvider>
   );
 }
